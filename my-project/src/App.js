87c0dc29f6842a22d78efd65307078275f56d187
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-09-02 01:02:34 +0530

    First commit

---
+++ my-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='bg-yellow-400'>
+        Hello world
+      </p>
+    </div>
+  );
+}
+
+export default App;
